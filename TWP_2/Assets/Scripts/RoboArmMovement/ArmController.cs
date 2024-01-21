dc93b23e816c08a440f2df2fbc92884fb5e4bee0
TheSausageSwiper <97609792+onenicktwo@users.noreply.github.com>
2024-01-20 21:04:47 -0600

    Magnet Arm
    Very simple magnet arm controller. When object is stuck it can go through the floor, not sure how to fix this.

--- TWP_2/Assets/Scripts/RoboArmMovement/ArmController.cs
+++ TWP_2/Assets/Scripts/RoboArmMovement/ArmController.cs
@@ -5,29 +5,28 @@ using UnityEngine.UI;
 
 public class ArmController : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject stuckObject;
+
     public Slider baseSlider;
     public Slider lowerArmSlider;
     public Slider upperArmSlider;
     public Slider neckHingeSlider;
-    public Slider neckSwivelSlider;
 
     private float baseSliderValue = 0f;
     private float lowerArmSliderValue = 0f;
     private float upperArmSliderValue = 0f;
     private float neckHingeSliderValue = 0f;
-    private float neckSwivelSliderValue = 0f;
 
-    public Rigidbody robotBase;
-    public Rigidbody robotLowerArm;
-    public Rigidbody robotUpperArm;
-    public Rigidbody robotNeckHinge;
-    public Rigidbody robotNeckSwivel;
+    public Transform robotBase;
+    public Transform robotLowerArm;
+    public Transform robotUpperArm;
+    public Transform robotNeckHinge;
 
     public float baseTurnRate = 1f;
     public float lowerArmTurnRate = 1f;
     public float upperArmTurnRate = 1f;
     public float neckHingeTurnRate = 1f;
-    public float neckSwivelTurnRate = 1f;
 
     private float baseYRot = 0f;
     public float baseYRotMin = -45f;
@@ -45,28 +44,22 @@ public class ArmController : MonoBehaviour
     public float neckHingeXRotMin = -45f;
     public float neckHingeXRotMax = 45f;
 
-    private float neckSwivelYRot = 0f;
-    public float neckSwivelYRotMin = -180f;
-    public float neckSwivelYRotMax = 180f;
-
     private void Start()
     {
         baseSlider.minValue = -1;
         lowerArmSlider.minValue = -1;
         upperArmSlider.minValue = -1;
         neckHingeSlider.minValue = -1;
-        neckSwivelSlider.minValue = -1;
 
         baseSlider.maxValue = 1;
         lowerArmSlider.maxValue = 1;
         upperArmSlider.maxValue = 1;
         neckHingeSlider.maxValue = 1;
-        neckSwivelSlider.maxValue = 1;
 
         ResetSliders();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         CheckInput();
         ProcessMovement();
@@ -78,30 +71,25 @@ public class ArmController : MonoBehaviour
         lowerArmSliderValue = lowerArmSlider.value;
         upperArmSliderValue = upperArmSlider.value;
         neckHingeSliderValue = neckHingeSlider.value;
-        neckSwivelSliderValue = neckSwivelSlider.value;
     }
 
     void ProcessMovement()
     {
         baseYRot += baseSliderValue * baseTurnRate;
         baseYRot = Mathf.Clamp(baseYRot, baseYRotMin, baseYRotMax);
-        robotBase.rotation = Quaternion.Euler(robotBase.rotation.x, baseYRot, robotBase.rotation.z);
+        robotBase.localEulerAngles = new Vector3(robotBase.localEulerAngles.x, baseYRot, robotBase.localEulerAngles.z);
 
         lowerArmXRot += lowerArmSliderValue * lowerArmTurnRate;
         lowerArmXRot = Mathf.Clamp(lowerArmXRot, lowerArmXRotMin, lowerArmXRotMax);
-        robotLowerArm.rotation = Quaternion.Euler(lowerArmXRot, robotLowerArm.rotation.y, robotLowerArm.rotation.z);
+        robotLowerArm.localEulerAngles = new Vector3(lowerArmXRot, robotLowerArm.localEulerAngles.y, robotLowerArm.localEulerAngles.z);
 
         upperArmXRot += upperArmSliderValue * upperArmTurnRate;
         upperArmXRot = Mathf.Clamp(upperArmXRot, upperArmXRotMin, upperArmXRotMax);
-        robotUpperArm.rotation = Quaternion.Euler(upperArmXRot, robotUpperArm.rotation.y, robotUpperArm.rotation.z);
+        robotUpperArm.localEulerAngles = new Vector3(upperArmXRot, robotUpperArm.localEulerAngles.y, robotUpperArm.localEulerAngles.z);
 
         neckHingeXRot += neckHingeSliderValue * neckHingeTurnRate;
         neckHingeXRot = Mathf.Clamp(neckHingeXRot, neckHingeXRotMin, neckHingeXRotMax);
-        robotNeckHinge.rotation = Quaternion.Euler(neckHingeXRot, robotNeckHinge.rotation.y, robotNeckHinge.rotation.z);
-
-        neckSwivelYRot += neckSwivelSliderValue * neckSwivelTurnRate;
-        neckSwivelYRot = Mathf.Clamp(neckSwivelYRot, neckSwivelYRotMin, neckSwivelYRotMax);
-        robotNeckSwivel.rotation = Quaternion.Euler(robotNeckSwivel.rotation.x, neckSwivelYRot, robotNeckSwivel.rotation.z);
+        robotNeckHinge.localEulerAngles = new Vector3(neckHingeXRot, robotNeckHinge.localEulerAngles.y, robotNeckHinge.localEulerAngles.z);
     }
 
     public void ResetSliders()
@@ -110,12 +98,10 @@ public class ArmController : MonoBehaviour
         lowerArmSliderValue = 0f;
         upperArmSliderValue = 0f;
         neckHingeSliderValue = 0f;
-        neckSwivelSliderValue = 0f;
 
         baseSlider.value = 0f;
         lowerArmSlider.value = 0f;
         upperArmSlider.value = 0f;
         neckHingeSlider.value = 0f;
-        neckSwivelSlider.value = 0f;
     }
 }
