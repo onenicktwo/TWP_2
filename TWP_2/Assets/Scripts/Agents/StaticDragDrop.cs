f86c4592280e70ae8684e26c30032379a25762c1
Adam Ahmetasevic <adamahmetasevic@gmail.com>
2024-01-28 21:33:44 -0600

    Created Scripts
    Altered magnet to add bool
    Added Static Drag Drop Script(unfinished, first concept)

---
+++ TWP_2/Assets/Scripts/Agents/StaticDragDrop.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class StaticDragDrop : Agent
+{
+    [SerializeField] private Material winMat;
+    [SerializeField] private Material failMat;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
+    [SerializeField] private EnvironmentManager envManager;
+    [SerializeField] private Transform goalTransform;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float pickUpRange = 1.5f; // Range within which the agent can pick up objects
+    private GameObject objectInHand;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(0, 55, 0);
+        ResetObjects();
+    }
+
+    private void ResetObjects()
+    {
+        goalTransform.localPosition = new Vector3(Random.Range(4f, 1f), 1, Random.Range(-1.5f, 1.5f));
+
+        if (objectInHand != null)
+        {
+            Destroy(objectInHand);
+            objectInHand = null;
+        }
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(goalTransform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+        float moveZ = actions.ContinuousActions[2];
+
+        transform.localPosition += new Vector3(moveX, moveY, moveZ) * Time.deltaTime * moveSpeed;
+
+        if (objectInHand != null)
+        {
+            objectInHand.transform.localPosition += new Vector3(moveX, moveY, moveZ) * Time.deltaTime * moveSpeed;
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> contActions = actionsOut.ContinuousActions;
+        contActions[0] = Input.GetAxisRaw("Horizontal");
+        contActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Object") && objectInHand == null)
+        {
+            //PickUpObject(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("GoalMat") && objectInHand != null)
+        {
+            AddReward(+1f);
+            floorMeshRenderer.material = winMat;
+            EndEpisode();
+        }
+        else if (other.gameObject.CompareTag("Wall"))
+        {
+            SetReward(-1f);
+            floorMeshRenderer.material = failMat;
+            EndEpisode();
+        }
+    }
+
+
+}
--- TWP_2/Assets/Scripts/RoboArmMovement/Magnet.cs
+++ TWP_2/Assets/Scripts/RoboArmMovement/Magnet.cs
@@ -6,12 +6,14 @@ public class Magnet : MonoBehaviour
 {
     public ArmController armController;
     public GameObject parent;
-
+    
+    public bool isPickedUp = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Object")
         {
+            isPickedUp=true;
             armController.SetStuckObject(other.gameObject);
             other.gameObject.transform.parent = parent.transform;
             Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
