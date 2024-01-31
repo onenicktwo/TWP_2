77dfea88ada646e6837751da90a63738cd49a889
TheSausageSwiper <97609792+onenicktwo@users.noreply.github.com>
2024-01-30 22:27:58 -0600

    Testing Dynamic Touch Model

---
+++ TWP_2/Assets/Scripts/Agents/DynamicTouchAgent.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class DynamicTouchAgent : Agent
+{
+    [SerializeField]
+    private Material winMat;
+    [SerializeField]
+    private Material failMat;
+    [SerializeField]
+    private MeshRenderer floorMeshRenderer;
+
+    [SerializeField]
+    private EnvironmentManager envManager;
+
+    [SerializeField]
+    private Transform goalTransform;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(0, 55, 0);
+    
+        Vector3 spawnPosition = Random.insideUnitCircle.normalized;
+        spawnPosition *= Random.Range(GameManager.inst.MinDist, GameManager.inst.MaxDist);
+        goalTransform.localPosition = new Vector3(spawnPosition.x, 3f, spawnPosition.y);
+    
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(goalTransform.localPosition);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+        float moveZ = actions.ContinuousActions[2];
+
+        transform.localPosition += new Vector3(moveX, moveY, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> contActions = actionsOut.ContinuousActions;
+        // Can add Y if needed
+        contActions[0] = Input.GetAxisRaw("Horizontal");
+        contActions[2] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Object")
+        {
+            Debug.Log("Win");
+            AddReward(+1f);
+            floorMeshRenderer.material = winMat;
+            EndEpisode();
+        }
+        else if (other.gameObject.tag == "Wall")
+        {
+            SetReward(-1f);
+            floorMeshRenderer.material = failMat;
+            EndEpisode();
+        }
+    }
+}
