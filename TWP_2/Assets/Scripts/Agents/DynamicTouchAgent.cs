983941b094382f6c4542e4ac51da862a4231ea10
TheSausageSwiper <97609792+onenicktwo@users.noreply.github.com>
2024-02-06 23:49:11 -0600

    Dynamic Touch
    On DynamicTouch14
    Trying out different configs, the newest one seems to show some promise. However it took many iterations for it to get to that point, it may be to slow to take advantage of. Will try giving more rewards to quicken the process.

--- TWP_2/Assets/Scripts/Agents/DynamicTouchAgent.cs
+++ TWP_2/Assets/Scripts/Agents/DynamicTouchAgent.cs
@@ -23,6 +23,9 @@ public class DynamicTouchAgent : Agent
     [SerializeField]
     private float moveSpeed = 1f;
 
+    private float beginDistance;
+    private float prevBest;
+
     public override void OnEpisodeBegin()
     {
         transform.localPosition = new Vector3(0, 55, 0);
@@ -30,13 +33,18 @@ public class DynamicTouchAgent : Agent
         Vector3 spawnPosition = Random.insideUnitCircle.normalized;
         spawnPosition *= Random.Range(GameManager.inst.MinDist, GameManager.inst.MaxDist);
         goalTransform.localPosition = new Vector3(spawnPosition.x, 3f, spawnPosition.y);
-    
+
+        beginDistance = Vector3.Distance(transform.localPosition, goalTransform.localPosition);
+        prevBest = beginDistance;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.localPosition);
         sensor.AddObservation(goalTransform.localPosition);
+        //Vector3 toComponent = (goalTransform.localPosition - transform.localPosition);
+        //sensor.AddObservation(toComponent);
+        //sensor.AddObservation(Vector3.Distance(goalTransform.localPosition, transform.localPosition));
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
@@ -45,6 +53,20 @@ public class DynamicTouchAgent : Agent
         float moveZ = actions.ContinuousActions[2];
 
         transform.localPosition += new Vector3(moveX, moveY, moveZ) * Time.deltaTime * moveSpeed;
+
+        /*
+        float distance = Vector3.Distance(transform.localPosition, goalTransform.localPosition);
+        float diff = beginDistance - distance;
+        if (distance > prevBest)
+        {
+            AddReward(prevBest - distance);
+        }
+        else
+        {
+            AddReward(diff);
+            prevBest = distance;
+        }
+        */
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -60,13 +82,13 @@ public class DynamicTouchAgent : Agent
         if (other.gameObject.tag == "Object")
         {
             Debug.Log("Win");
-            AddReward(+1f);
+            AddReward(+0.5f);
             floorMeshRenderer.material = winMat;
             EndEpisode();
         }
         else if (other.gameObject.tag == "Wall")
         {
-            SetReward(-1f);
+            AddReward(-1f);
             floorMeshRenderer.material = failMat;
             EndEpisode();
         }
